3fc96fd5203e95dac56f480501f4ed4ef54f3073
kgbzoma <kgbzoma@gmail.com>
2016-05-28 15:47:20 +0200

    Głupi program

---
+++ Warcaby/Warcaby/WarcabyPresenter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warcaby
+{
+    class WarcabyPresenter
+    {
+        public WarcabyPresenter(WarcabyView view, Warcaby model)
+        {
+            /*this.view = view;
+            this.model = model;
+            view.CommandPressed += View_CommandPressed;
+            view.DigitPressed += View_DigitPressed;
+
+            InitializeHandlers();*/
+        }
+    }
+}
