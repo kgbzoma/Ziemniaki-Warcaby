f0958ce7e335e44a907b75a4493c3b690832c21f
gabibibi <a.gabryel95@gmail.com>
2016-06-22 10:23:48 +0200

    Agata
    Dodanie niuansów

--- Warcaby/Warcaby/Warcaby.cs
+++ Warcaby/Warcaby/Warcaby.cs
@@ -17,17 +17,17 @@ namespace Warcaby
         //Sprawdzanie czy ruch jest mozliwy ?
         // Sprawdzanie czy bicie jest mozliwe?
         // Kolejnosc. Prezenter przekazuje mi ruch. Ja sprawdzam czy idzie wykonać bicie dla jakiegokolwiek innego pionka. Jesli tak ORAZ to jest wlasnie ten ruch co mi pokazał tamten gracz to wykonuje 
-        public bool isMovePossible(Pole stare,Pole nowe,Gracz graczKtoregoSprawdzam)
-        {
+        //public bool isMovePossible(Pole stare,Pole nowe,Gracz graczKtoregoSprawdzam)
+        //{
             //Czy można ruch na to pole ?
             //Czy można zbić to pole?
             //Czy można ruszyć się w tą stronę
-            if(graczKtoregoSprawdzam.isPionekonPole(stare))
-            {
+          //  if(graczKtoregoSprawdzam.isPionekPole(stare))
+           // {
                // sadas
-            }
-            return true;
-        }
+          //  }
+           // return true;
+       // }
         public bool moveWayPossible(Pole stare,Pole nowe)
         {
             if(graczPrzyKolejce==player)
--- Warcaby/Warcaby/WarcabyPresenter.cs
+++ Warcaby/Warcaby/WarcabyPresenter.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Drawing;
 
 namespace Warcaby
 {
@@ -11,6 +12,18 @@ namespace Warcaby
 
         private Warcaby model;
         private WarcabyView view;
+        private  static Szachownica gameBoard = new Szachownica();
+        private Gracz player = new Gracz(Color.White, true, gameBoard);
+        private Gracz aiPlayer = new Gracz(Color.Black, false, gameBoard);
+        private Gracz graczPrzyKolejce;
+
+
+
+        public WarcabyPresenter (WarcabyView view)
+        {
+            this.view = view;
+           
+        }
 
 
         private WarcabyView View
