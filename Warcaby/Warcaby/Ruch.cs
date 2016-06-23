8b4a6e4c8c55da49bdaa068713769ddcb63cef50
gabibibi <a.gabryel95@gmail.com>
2016-06-23 09:01:33 +0200

    Agata
    Dodanie klas

---
+++ Warcaby/Warcaby/Ruch.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Warcaby
+{
+    class Ruch
+    {
+        private List<int> pozycje = new List<int>();
+
+        #region Konstruktory
+        public Ruch() { }
+        public Ruch(int pierwszy, params int[] czekaj)
+        {
+
+        }
+
+
+        #endregion
+
+       // public void DodajRuch()
+    }
+}
